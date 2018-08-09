d8ba9d00803657b02b914641b32cc848f04de0c0
ERICSSON\eqinkef <kefu.qin@ericsson.com>
2018-08-09 11:34:56 +0800

    update

--- file-operator/src/main/java/Test.java
+++ file-operator/src/main/java/Test.java
@@ -12,13 +12,13 @@ public class Test {
 
 	private static Logger LOGGER = Logger.getLogger(Test.class);
 
-	static String filePath = "C:/local_data/upload/Policy_StatisticsTable_LN_s12343423423eq_20180808160945.csv";
+	static String filePath = "C:/local_data/upload/test.csv";
 	static String extra_csv_str = "";
 	static String COMMA_FOR_STR = ",";
 	static String path = "c:/local_data/upload/";
 	static String fileName = "writer.csv";
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		List<Map<String, String>> csvList = new ArrayList<Map<String, String>>();
@@ -44,113 +44,6 @@ public class Test {
 		savePropFile(propMap);
 		saveCsvFile(csvList, fileName);
 
-
-
-	}
-
-	/**
-	 * @param csvList
-	 * @param fileName
-	 * @throws Exception
-	 * @desc 保存CSV文件
-	 */
-	public static void saveCsvFile(List<Map<String, String>> csvList, String fileName) throws Exception {
-
-		LOGGER.debug("[saveCsvFile] : Save csv file.");
-
-		BufferedWriter bw = null;
-		try {
-			File csvFolder = new File(path);
-			if (!csvFolder.exists()) {
-				csvFolder.mkdirs();
-			}
-
-			File csv = new File(path + fileName); // CSV文件
-
-			if (!csv.exists()) {
-				csv.createNewFile();
-			}/* else {
-				csv.delete();
-				csv.createNewFile();
-			}*/
-
-			// true-追加模式   false-清空模式
-			bw = new BufferedWriter(new FileWriter(csv, false));
-
-			for (Map<String, String> map : csvList) {
-				// 新增一行数据
-				bw.write(map.get("code") + COMMA_FOR_STR + map.get("num") + COMMA_FOR_STR + map.get("others"));
-				bw.newLine();
-			}
-
-		} catch (FileNotFoundException e) {
-			LOGGER.error("File is not found.");
-			LOGGER.error(e.getMessage());
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage());
-		} finally {
-			if (bw != null) {
-				try {
-					bw.close();
-				} catch (IOException e) {
-					System.out.println(e.getMessage());
-				}
-			}
-
-		}
-	}
-
-	/**
-	 * @param propMap
-	 * @param csvAttachList
-	 * @param csvMap
-	 * @param addStr
-	 * @desc // 循环prop文件信息，与csv文件的配置对比 <br/>
-	// 记录prop中存在，csv中不存在的policy_code和num信息
-	 */
-	public static void parsePropAndCsvAttach(Map<String, String> propMap, List<Map<String, String>> csvAttachList, Map<String, String> csvMap, String addStr) {
-
-		LOGGER.debug("[parsePropAndCsvAttach] : Compare prop and csv file, get the extra prop info.");
-
-		Iterator<Map.Entry<String, String>> entries = propMap.entrySet().iterator();
-		Map<String, String> tempMap = new HashMap<String, String>();
-		while (entries.hasNext()) {
-			Map.Entry<String, String> entry = entries.next();
-			String propCode = entry.getKey();
-			String propNum = entry.getValue();
-			if (csvMap.get(propCode) == null) {
-				tempMap = new HashMap<String, String>();
-				tempMap.put("code", propCode);
-				tempMap.put("num", propNum);
-				tempMap.put("others", addStr);
-				csvAttachList.add(tempMap);
-			}
-		}
-
-	}
-
-	/**
-	 * @param csvList
-	 * @param propMap
-	 * @param propAttachMap
-	 * @desc // 循环csv文件信息，与prop文件的配置对比，如果有相同的code，则将num值相加，并更新到num <br/>
-	// 记录csv中存在,prop中不存在的policy_code和num信息
-	 */
-	public static void parseCsvAndPropAttach(List<Map<String, String>> csvList, Map<String, String> propMap, Map<String, String> propAttachMap) {
-
-		LOGGER.debug("[parseCsvAndPropAttach] : Compare csv and prop file, get the extra csv info.");
-
-		for (Map<String, String> map : csvList) {
-			String csv_code = map.get("code").toString();
-			int csv_num = Integer.parseInt(map.get("num"));
-			if (propMap.get(csv_code) != null) {
-				String totalNum = String.valueOf(csv_num + Integer.parseInt(propMap.get(csv_code)));
-				map.put("num", totalNum);
-				propMap.put(csv_code, totalNum);
-			} else {
-				propAttachMap.put(csv_code, String.valueOf(csv_num));
-			}
-		}
 	}
 
 	/**
@@ -199,7 +92,11 @@ public class Test {
 					map = new HashMap<String, String>();
 					map.put("code", formatPolicyCode(item[0]));
 					map.put("num", item[1]);
-					map.put("others", line.substring(line.indexOf(item[1]) + item[1].length() + 1));
+					String others = "";
+					if (item.length > 2) {
+						others = line.substring(item[0].length() + item[1].length() + 2);
+					}
+					map.put("others", others);
 					csvList.add(map);
 				}
 			}
@@ -210,34 +107,6 @@ public class Test {
 			LOGGER.error(e.getMessage());
 		}
 		return csvList;
-	}
-
-
-
-	/**
-	 * @return Map<String, String>
-	 * @desc 获取Csv中policy_code和num组成Map
-	 */
-	public static Map<String, String> getCsvMap() {
-
-		LOGGER.debug("[getCsvMap] : Read csv file, put policy_code/num to map");
-
-		Map<String, String> csvMap = new HashMap<String, String>();
-
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filePath));//换成你的文件名
-			String line = null;
-			while((line=reader.readLine())!=null){
-				String[] item = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
-				csvMap.put(formatPolicyCode(item[0]), item[1]);
-			}
-		} catch (FileNotFoundException e) {
-			LOGGER.error("File is not found.");
-			LOGGER.error(e.getMessage());
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage());
-		}
-		return csvMap;
 	}
 
 	/**
@@ -266,8 +135,86 @@ public class Test {
 	}
 
 	/**
+	 * @return Map<String, String>
+	 * @desc 获取Csv中policy_code和num组成Map
+	 */
+	public static Map<String, String> getCsvMap() {
+
+		LOGGER.debug("[getCsvMap] : Read csv file, put policy_code/num to map");
+
+		Map<String, String> csvMap = new HashMap<String, String>();
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));//换成你的文件名
+			String line = null;
+			while((line=reader.readLine())!=null){
+				String[] item = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
+				csvMap.put(formatPolicyCode(item[0]), item[1]);
+			}
+		} catch (FileNotFoundException e) {
+			LOGGER.error("File is not found.");
+			LOGGER.error(e.getMessage());
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+		}
+		return csvMap;
+	}
+
+	/**
+	 * @param csvList
 	 * @param propMap
-	 * @throws Exception
+	 * @param propAttachMap
+	 * @desc // 循环csv文件信息，与prop文件的配置对比，如果有相同的code，则将num值相加，并更新到num <br/>
+	// 记录csv中存在,prop中不存在的policy_code和num信息
+	 */
+	public static void parseCsvAndPropAttach(List<Map<String, String>> csvList, Map<String, String> propMap, Map<String, String> propAttachMap) {
+
+		LOGGER.debug("[parseCsvAndPropAttach] : Compare csv and prop file, get the extra csv info.");
+
+		for (Map<String, String> map : csvList) {
+			String csv_code = map.get("code").toString();
+			int csv_num = Integer.parseInt(map.get("num"));
+			if (propMap.get(csv_code) != null) {
+				String totalNum = String.valueOf(csv_num + Integer.parseInt(propMap.get(csv_code)));
+				map.put("num", totalNum);
+				propMap.put(csv_code, totalNum);
+			} else {
+				propAttachMap.put(csv_code, String.valueOf(csv_num));
+			}
+		}
+	}
+
+	/**
+	 * @param propMap
+	 * @param csvAttachList
+	 * @param csvMap
+	 * @param addStr
+	 * @desc // 循环prop文件信息，与csv文件的配置对比 <br/>
+	// 记录prop中存在，csv中不存在的policy_code和num信息
+	 */
+	public static void parsePropAndCsvAttach(Map<String, String> propMap, List<Map<String, String>> csvAttachList, Map<String, String> csvMap, String addStr) {
+
+		LOGGER.debug("[parsePropAndCsvAttach] : Compare prop and csv file, get the extra prop info.");
+
+		Iterator<Map.Entry<String, String>> entries = propMap.entrySet().iterator();
+		Map<String, String> tempMap = new HashMap<String, String>();
+		while (entries.hasNext()) {
+			Map.Entry<String, String> entry = entries.next();
+			String propCode = entry.getKey();
+			String propNum = entry.getValue();
+			if (csvMap.get(propCode) == null) {
+				tempMap = new HashMap<String, String>();
+				tempMap.put("code", propCode);
+				tempMap.put("num", propNum);
+				tempMap.put("others", addStr);
+				csvAttachList.add(tempMap);
+			}
+		}
+
+	}
+
+	/**
+	 * @param propMap
 	 * @desc 保存属性到properties文件
 	 */
 	public static void savePropFile(Map<String, String> propMap) {
@@ -310,18 +257,55 @@ public class Test {
 		}
 	}
 
-	public static Properties loadConfig(String filePath) {
-		Properties props = new Properties();
-		InputStream in;
+	/**
+	 * @param csvList
+	 * @param fileName
+	 * @desc 保存CSV文件
+	 */
+	public static void saveCsvFile(List<Map<String, String>> csvList, String fileName) {
+
+		LOGGER.debug("[saveCsvFile] : Save csv file.");
+
+		BufferedWriter bw = null;
 		try {
-			in = new FileInputStream(filePath);
-			props.load(in);
+			File csvFolder = new File(path);
+			if (!csvFolder.exists()) {
+				csvFolder.mkdirs();
+			}
+
+			File csv = new File(path + fileName); // CSV文件
+
+			if (!csv.exists()) {
+				csv.createNewFile();
+			}/* else {
+				csv.delete();
+				csv.createNewFile();
+			}*/
+
+			// true-追加模式   false-清空模式
+			bw = new BufferedWriter(new FileWriter(csv, false));
+
+			for (Map<String, String> map : csvList) {
+				// 新增一行数据
+				bw.write(map.get("code") + COMMA_FOR_STR + map.get("num") + COMMA_FOR_STR + map.get("others"));
+				bw.newLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			LOGGER.error("File is not found.");
+			LOGGER.error(e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
+			LOGGER.error(e.getMessage());
+		} finally {
+			if (bw != null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+
 		}
-		return props;
 	}
 
 	/**
@@ -336,6 +320,20 @@ public class Test {
 			return policyCodeLen > 32 ? policyCode.substring(policyCodeLen - 32, policyCodeLen) : policyCode;
 		}
 
+	}
+
+	public static Properties loadConfig(String filePath) {
+		Properties props = new Properties();
+		InputStream in;
+		try {
+			in = new FileInputStream(filePath);
+			props.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return props;
 	}
 
 }
