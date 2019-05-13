31f6251d4cde5d0b4da066b71d02d82e87411cbe
ERICSSON\eqinkef <kefu.qin@ericsson.com>
2019-05-13 10:20:19 +0800

    java8 stream() 对字符串排序、去重

--- file-operator/src/main/java/com/felix/java8new/SortFilter.java
+++ file-operator/src/main/java/com/felix/java8new/SortFilter.java
@@ -1,10 +1,22 @@
 package com.felix.java8new;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SortFilter {
 
     public static void main(String[] args) {
 
         String str = "abcdedafdsadf";
+        List<String> list = new ArrayList<>();
+
+        for (char c : str.toCharArray()) {
+            list.add(String.valueOf(c));
+        }
+
+        List newList = list.stream().distinct().sorted().collect(Collectors.toList());
+        System.out.println(String.join("", newList));
 
     }
 
